8d0ca4d2dc0490033e7d845c29b99290823ec0ac
JustFun <paw93@ukr.net>
2026-03-26 13:51:29 +0200

    +++

---
+++ app/notes/filter/@sidebar/default.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import css from "./SideBarNotes.module.css"
+import { noteTags } from "@/types/note"
+
+export default function SidebarNotes() {
+    return (
+         <ul className={css.menuList}>
+      <li className={css.menuItem}>
+        <Link href={`/notes/filter/all`} className={css.menuLink}>
+          All notes
+        </Link>
+      </li>
+      {noteTags.map((noteTag) => (
+        <li className={css.menuItem} key={noteTag}>
+          <Link href={`/notes/filter/${noteTag}`} className={css.menuLink}>
+            {noteTag}
+          </Link>
+        </li>
+      ))}
+    </ul>
+    )
+}
